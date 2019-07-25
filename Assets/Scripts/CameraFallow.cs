21f353891c78df66ceff6a632c168576481886a5
zmienny <kamil.roszak98@gmail.com>
2019-07-26 00:31:55 +0200

    Add lights and particles

---
+++ Assets/Scripts/CameraFallow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFallow : MonoBehaviour
+{
+    public Transform target;
+    public float cameraSpeed = 5f;
+
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, cameraSpeed * Time.deltaTime * Vector2.Distance(transform.position, target.position));
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+}
